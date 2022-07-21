3ea45a88411df1ef5aabf7dcf78e1bf8b9604653
Anton Zubov QAMID-42 <bakaphoton@gmail.com>
2022-07-21 15:22:36 +0300

    zadanie1

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+
+        int ticket = 6000;
+        int price = 20;
+        int bonus = ticket / price;
+        System.out.println(bonus);
+
+    }
+}
